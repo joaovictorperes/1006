c60b1d9db8c54cff81a4871c0ed8e0651ea3c4bf
João Victor <78619731+joaovictorperes@users.noreply.github.com>
2021-05-29 17:09:40 -0300

    Update 1010 .py

--- 1010 .py
+++ 1010 .py
@@ -1,4 +1,5 @@
-"""Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago."""
+"""Neste problema, deve-se ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2,
+o número de peças 2 e o valor unitário de cada peça 2. Após, calcule e mostre o valor a ser pago."""
 
 num1,quantidade1,preco1 = input().split(" ") # pega 3 valores na mesma linha e atribui a variáveis
 num2,quantidade2,preco2 = input().split(" ") # pega 3 valores na mesma linha e atribui a variáveis
